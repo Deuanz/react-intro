3861c97f1c90c76a73f722fc543c09418d4b0454
deuanz <deuanz.cpenu@gmail.com>
2016-10-15 12:04:11 +0700

    Display dynamic list

--- src/app.js
+++ src/app.js
@@ -2,10 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const title = "My React App"
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">Search</button>
+        </form>
+    )
+}
+const Header = (props) => (
+    <header className="app-container">
+        <h1>{props.title}</h1>
+        <SearchForm />
+    </header>
+)
 
+const Items = (props) => {
+    console.log(props.items)
+    return (
+    <ul>
+        {
+            props.items.map(item => (
+                <li>{item}</li>
+            ))
+        }
+    </ul>
+    )
+}
+
+const Content = (props) => (
+    <section>
+        <p>{props.description}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+const AppWithOutDescription = () => (
+    <Header title="App with out discription"></Header>
+)
 const App = () => {
-    return <div>MY REACT APP</div>
+    const title = 'Fronttechs: React'
+    const content = 'This is a simple react application ^^'
+    const items = [
+        "Oliver",
+        "Tobey",
+        "Charlie",
+        "Lucky",
+        "Poyo"
+    ]
+    return (
+        <section>
+            <Header title={title} />
+            <Content
+                description={content}
+                items={items} />
+        </section>
+    )
 }
 
 const element = document.getElementById('app')
