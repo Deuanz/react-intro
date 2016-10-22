b0905c55b8e7dcd76696cb08a1763cd2dc86ccda
deuanz <deuanz.cpenu@gmail.com>
2016-10-22 11:07:12 +0700

    Search form to a new file

--- src/app.js
+++ src/app.js
@@ -1,34 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {SearchForm} from './search-form'
 
-
-class SearchForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            query : ''
-        }
-    }
-    onSearchClick(event) {
-        event.preventDefault()
-        console.log("this.onSearchClick", this.state.query)
-    }
-    onQueryChange(event) {
-        const query = event.target.value
-        console.log('this.onQueryChange', query)
-        this.setState({
-            query: query
-        })
-    }
-    render () {
-        return (
-            <form>
-                <input type="text" value={this.state.query} onChange={this.onQueryChange.bind(this)} />
-                <button onClick={this.onSearchClick.bind(this)}>Search</button>
-            </form>
-        )
-    }
-}
 const MovieList = (props) => (
     <ul>
     {props.movies.map((movie, i) => {
